f3dcaf2d66ad4cb937eabf8428fa0e62740448c4
Mikołaj <mikolaj.gorczynski@student.wat.edu.pl>
2020-05-13 10:23:19 +0200

    4.8.2 Dodawanie książek do koszyka

--- klasy/Basket.php
+++ klasy/Basket.php
@@ -9,7 +9,27 @@ $_SESSION['basket'] = array();
 }
 }
 function add() {
-}
+    // Sprawdzenie poprawności parametru id
+    if (!isset($_GET['id'])) {
+    return FORM_DATA_MISSING;
+    }
+    if (($id = (int) $_GET['id']) < 1) {
+    return INVALID_ID;
+    }
+    // Sprawdzenie, czy istnieje książka o podanym id
+    $query = "SELECT Cena FROM ksiazki WHERE id=$id";
+    if (($cena = $this->dbo->getQuerySingleResult($query)) === false) {
+    return INVALID_ID;
+    }
+    // Zapisanie identyfikatora książki w koszyku
+    if (isset($_SESSION['basket'][$id])) {
+    $_SESSION['basket'][$id]->ile++;
+    $_SESSION['basket'][$id]->cena = $cena;
+    } else {
+    $_SESSION['basket'][$id] = new BasketItem($id, $cena, 1);
+    }
+    return ACTION_OK;
+    }
 function show($title, $allowModify = true) {
 }
 function modify() {
